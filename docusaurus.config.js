3ad2ee7b1919ee8474b21221c8604f0499b166d6
sunmodza <thaphon.c@ku.th>
2026-02-27 17:14:16 +0700

    Fix baseUrl to match repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://your-username.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pm/',
+  baseUrl: '/pm_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
